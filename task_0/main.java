0c528ba4d145ff21293532329556ee85577e7d9c
yvrqqd <88439148+yvrqqd@users.noreply.github.com>
2022-02-15 15:02:24 +0700

    Update main.java

--- task_0/main.java
+++ task_0/main.java
@@ -2,33 +2,30 @@ package task_0;
 
 import java.io.*;
 import java.lang.StringBuilder;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
 
 public class Main {
-    private static final String IN_FILE_NAME = "/home/qqd/IdeaProjects/task1/out/production/task1/task1/in.txt";
+    private static final String IN_FILE_NAME = "in.txt";
     private static final String OUT_FILE_NAME = "out.txt";
 
     private static void compute_freq(String in_file, String out_file) throws IOException {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
 
         Reader reader = null;
         reader = new InputStreamReader(new FileInputStream(in_file));
 
+        int letter;
         int total_count = 0;
         StringBuilder tmp_str = new StringBuilder();
 
-        while (true) {
-            int letter = reader.read();
-            if (letter == -1) {
-                break;
-            }
+        while ((letter = reader.read()) != -1) {
             while (Character.isLetterOrDigit(letter)) {
                 tmp_str.append((char) letter);
                 letter = reader.read();
             }
             if (tmp_str.length() == 0) {
-                break;
+                continue;
             }
             if (map.containsKey(tmp_str.toString())) {
                 map.put(tmp_str.toString(), map.get(tmp_str.toString()) + 1);
@@ -39,9 +36,23 @@ public class Main {
             tmp_str.setLength(0);
         }
 
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((a, b) -> {
+            Integer v1 = a.getValue();
+            Integer v2 = b.getValue();
+            int cmp = v2.compareTo(v1);
+
+            if (cmp == 0) {
+                String s1 = a.getKey();
+                String s2 = b.getKey();
+                cmp = s1.compareTo(s2);
+            }
+            return cmp;
+        });
+
         BufferedWriter output = new BufferedWriter(new FileWriter(out_file));
-        for (var i : map.keySet()) {
-            output.write(i + ',' + map.get(i) + ',' + (float) map.get(i) / (float) total_count * 100 + "%\n");
+        for (Map.Entry<String, Integer> i : list) {
+            output.write(i.getKey() + ',' + (float) i.getValue() / (float) total_count * 100 + "%\n");
         }
         output.close();
     }
